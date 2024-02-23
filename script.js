68cb96d21c5994713a887fb8b7c5c0228ef033aa
Frances <135648147+fancypog@users.noreply.github.com>
2024-02-23 20:12:27 +0000

    Fix a typo in loop of grid generation

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const rows = 16;
 const columns = 16;
 
 
-for (let i = 0; i <= rows; i++) {
+for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('gridItem');
