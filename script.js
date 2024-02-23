d5129e2fd1a43d2d30fecb8cb957f8cd0484db7a
Frances <135648147+fancypog@users.noreply.github.com>
2024-02-23 13:25:50 +0000

    Add class to gridItems for assigning hov effect

--- script.js
+++ script.js
@@ -7,9 +7,8 @@ const columns = 16;
 for (let i = 0; i <= rows; i++) {
     for (let j = 0; j < columns; j++) {
         const gridItem = document.createElement('div');
-        gridItem.className = 'grid-item';
-        gridItem.textContent = 'Cell ${i+1}-${j+1}';
-
+        gridItem.classList.add('gridItem');
         gridContainer.appendChild(gridItem);
     }
 }
+
